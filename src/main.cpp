4521b7719a5d447e93a296b4a0b4ed5cd0c0ae24
Harry <harypatrascu.1@gmail.com>
2025-10-19 21:50:48 +0300

    barebones webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 #include <Arduino.h>
-#include <esp_camera.h>
 #include "WebServer.h"
 #include "WiFi.h"
+#include "esp_camera.h"
+#include "wifi_credentials.h"
 
 
 #define FLASH_PIN 4
 #define BAUD_RATE 115200
-#define CAMERA_MODEL_AI_THINKER
+
+
+WiFiServer server(80);
 
 
 void setup()
 {
     Serial.begin(BAUD_RATE);
-    pinMode(FLASH_PIN, OUTPUT);
+
+    // Wait for the serial port monitor to connect
+    while (!Serial);
+
+    Serial.print("Connecting to:");
+    Serial.println(WIFI_SSID);
+    WiFi.begin(WIFI_SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.print("Connected to wifi with IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
 }
 
 void loop()
 {
-    digitalWrite(FLASH_PIN, HIGH);
-    delay(500);
-    digitalWrite(FLASH_PIN, LOW);
-    delay(500);
+    WiFiClient client = server.available();
+    if (client) {
+        Serial.println("HTTP client connected");
+    }
 }
